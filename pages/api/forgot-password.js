ce675e7f47bed13d3865376409b4cd587d09b822
Shafi Akinropo <sakinropo@gmail.com>
2024-06-29 00:09:16 +0100

    update

--- pages/api/forgot-password.js
+++ pages/api/forgot-password.js
@@ -1,30 +1,31 @@
 import { sendResetPasswordEmail } from "@/lib/emailActions";
 import User from "@/models/User";
 import crypto from "crypto";
-import dbConnect from "@/utils/db"; // Assuming you have a Mongoose connection utility
+import dbConnect from "@/utils/db";
 
 export default async function handler(req, res) {
 	if (req.method === "POST") {
-		await dbConnect(); // Connect to MongoDB
+		await dbConnect();
 		const { email } = req.body;
-
 		try {
-			const user = await User.findOne({ email });
+			const resetToken = crypto.randomBytes(32).toString("hex");
+			const user = await User.findOneAndUpdate(
+				{ email },
+				{
+					$set: {
+						resetPasswordToken: crypto
+							.createHash("sha256")
+							.update(resetToken)
+							.digest("hex"),
+						resetPasswordExpires: Date.now() + 3600000,
+					},
+				},
+				{ returnNewDocument: true }
+			);
 			if (!user) {
 				return res.status(404).json({ message: "User not found" });
 			}
-
-			const resetToken = crypto.randomBytes(32).toString("hex");
-			user.resetPasswordToken = crypto
-				.createHash("sha256")
-				.update(resetToken)
-				.digest("hex");
-			user.resetPasswordExpires = Date.now() + 3600000; // 1 hour
-			await user.save();
-
-			// Send reset password email
 			await sendResetPasswordEmail(user, resetToken);
-
 			res.status(200).json({ message: "Password reset email sent" });
 		} catch (error) {
 			res.status(500).json({ error: error.message });
--- pages/api/reset-password.js
+++ pages/api/reset-password.js
@@ -1,32 +1,33 @@
 import User from "@/models/User";
 import crypto from "crypto";
-import dbConnect from "@/utils/db"; // Ensure Mongoose is connected
+import dbConnect from "@/utils/db";
 
 export default async function handler(req, res) {
 	if (req.method === "POST") {
-		await dbConnect(); // Ensure the database is connected
+		await dbConnect();
 		const { token, newPassword } = req.body;
-
 		try {
 			const hashedToken = crypto
 				.createHash("sha256")
 				.update(token)
 				.digest("hex");
-
-			const user = await User.findOne({
-				resetPasswordToken: hashedToken,
-				resetPasswordExpires: { $gt: Date.now() },
-			});
-
+			const user = await User.findOneAndUpdate(
+				{
+					resetPasswordToken: hashedToken,
+					resetPasswordExpires: { $gt: Date.now() },
+				},
+				{
+					$set: {
+						password: newPassword,
+						resetPasswordToken: null,
+						resetPasswordExpires: null,
+					},
+				},
+				{ returnNewDocument: true }
+			);
 			if (!user) {
 				return res.status(400).json({ message: "Invalid or expired token" });
 			}
-
-			user.password = newPassword;
-			user.resetPasswordToken = null;
-			user.resetPasswordExpires = null;
-			await user.save();
-
 			res.status(200).json({ message: "Password has been reset successfully" });
 		} catch (error) {
 			res.status(500).json({ error: error.message });
