a5a452111d916ae09354442ba6106f4ed45bfe59
Shafi Akinropo <sakinropo@gmail.com>
2024-06-29 07:31:21 +0100

    update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
+	assetPrefix: 'https://washman.onrender.com',
+  	publicRuntimeConfig: {BASE_URL: ' https://washman.onrender.com',},
 	images: {
 		remotePatterns: [
 			{
